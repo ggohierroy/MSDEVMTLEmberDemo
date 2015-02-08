e8ed687928ee97d9aeb70e845a31f96618c10765
Gabriel Gohier-Roy <ggohierroy@hotmail.com>
2015-02-08 14:08:42 -0500

    Added controllers

---
+++ MSDEVMTLEmberDemo/Controllers/PostContainer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MSDEVMTLEmberDemo.Models;
+
+namespace MSDEVMTLEmberDemo.Controllers
+{
+    public class PostContainer
+    {
+        public Post Post { get; set; }
+    }
+}
